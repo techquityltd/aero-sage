d735f322f8f79d83c40cd0825cf2c1072265d908
Chester Swann-Auger <chester@hey.com>
2022-08-31 15:41:00 +0100

    Added settings for heartbeat

--- src/SageServiceProvider.php
+++ src/SageServiceProvider.php
@@ -12,6 +12,7 @@ use Illuminate\Routing\Router;
 use Techquity\Aero\Sage\Console\Commands\UpdateProducts;
 use Techquity\Aero\Sage\Jobs\UpdateProduct;
 use Techquity\Aero\Sage\Listeners\CompletedOrder;
+use Illuminate\Support\Facades\Log;
 
 class SageServiceProvider extends ModuleServiceProvider
 {
@@ -43,6 +44,10 @@ class SageServiceProvider extends ModuleServiceProvider
             $group->array('currencies')->associative()->default(['GBP' => 1]);
             $group->integer('def_tax_code')->default(1);
             $group->string('def_nom_code')->default('4000');
+
+            $group->string('heartbeat_api_key');
+            $group->string('heartbeat_url');
+            $group->boolean('heartbeat_logging')->default('false');
 
             // Product import related settings
             $group->boolean('product_stock')
@@ -83,7 +88,28 @@ class SageServiceProvider extends ModuleServiceProvider
         $this->app->booted(static function () {
             $schedule = app(Schedule::class);
 
-            $schedule->job(new UpdateProduct, 'sage_50_import')->everyFiveMinutes()->withoutOverlapping();
+            $schedule->job(new UpdateProduct, 'sage_50_import')
+                ->everyFiveMinutes()
+                ->withoutOverlapping()
+                ->onSuccess(function () {
+                    if(setting('sage_50.heartbeat_logging')) {
+                        Log::debug('Sage Update Products Executed');
+                    }
+                    if(setting('sage_50.heartbeat_url')){
+
+                        $client = new \GuzzleHttp\Client();
+                        $url = setting('sage_50.heartbeat_url');
+                        $token = setting('sage_50.heartbeat_api_key');
+
+                        $headers = [
+                            'Authorization' => "Bearer $token",
+                        ];
+
+                        $request = new \GuzzleHttp\Psr7\Request('GET', $url, $headers);
+
+                        $client->sendAsync($request)->wait();
+                    }
+                });
         });
     }
 }
